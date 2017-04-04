7bd299e3f9e078310f3a664bffde7df256149593
Guenther Brunthaler <gb@xrpi3.emgenxx69lwyn5ctlr4nl64ul.local>
2017-04-04 23:36:11 +0200

    It seems to work now
    But as it turns out, gcc uses *strange* rather than big endianness when
    switching the CPU temporarily to big endian mode.

--- test_endianness.c
+++ test_endianness.c
@@ -1,55 +1,47 @@
-#if ASSUME_STDINT_H
+#if __STDC_VERSION__ >= 199901L
    #include <stdint.h>
-   typedef uintmax_t uint;
+   typedef uintmax_t biggest_uint;
 #else
-   typedef unsigned long uint;
+   typedef unsigned long biggest_uint;
 #endif
 #include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
-   unsigned bit, pupos, lpos;
-   #if ARM_ENABLE_BIGENDIAN
-      __asm("SETEND BE");
+   unsigned bit, lpos= UINT_MAX;
+   biggest_uint volatile var;
+   unsigned char volatile const *p= (void volatile const *)&var;
+   #if ARM_ENFORCE_BIGENDIAN
+      asm("setend be");
    #endif
-   pupos= lpos= UINT_MAX;
-   for (bit= sizeof(uint) * CHAR_BIT; bit--; ) {
-      uint const var= (uint)1 << bit;
-      unsigned char const *p= (void const *)&var;
-      unsigned found= UINT_MAX;
-      {
-         unsigned bytei;
-         for (bytei= sizeof var; bytei--; ) {
-            unsigned byte= p[bytei], bytebit;
-            for (bytebit= 1u << CHAR_BIT - 1; bytebit; bytebit>>= 1) {
-               if (byte & bytebit) {
-                  if (found != UINT_MAX) goto unsupported;
-                  found= bytei;
-               }
+   var= (biggest_uint)1;
+   #if ARM_ENFORCE_BIGENDIAN
+      asm("setend le");
+   #endif
+   for (bit= sizeof(biggest_uint) * CHAR_BIT; bit--; ) {
+      unsigned bytei, found= UINT_MAX;
+      for (bytei= sizeof var; bytei--; ) {
+         unsigned byte= p[bytei], bytebit;
+         for (bytebit= 1u << CHAR_BIT - 1; bytebit; bytebit>>= 1) {
+            if (byte & bytebit) {
+               if (found != UINT_MAX) abort();
+               found= bytei;
             }
          }
       }
-      if (found == UINT_MAX) goto unsupported;
+      #if ARM_ENFORCE_BIGENDIAN
+         asm("setend be");
+      #endif
+      var+= var;
+      #if ARM_ENFORCE_BIGENDIAN
+         asm("setend le");
+      #endif
+      if (found == UINT_MAX) abort();
       if (found == lpos) continue;
-      if (pupos != UINT_MAX) {
-         if (lpos - found != pupos - lpos) goto unsupported;
-      }
-      pupos= lpos;
+      if (printf("%s%u", lpos == UINT_MAX ? "" : "-", found) <= 0) abort();
       lpos= found;
    }
-   {
-      char rc; /* Will be unaffected by endianness. */
-      /* <lpos> is offset of least significant byte. */
-      if (lpos == 0 && pupos == 1) {
-         rc= 2; /* Little endian. */
-      } else if (lpos == pupos + 1 && lpos + 1 == sizeof(uint)) {
-         rc= 1; /* Big endian. */
-      } else {
-         unsupported:
-         rc= 0; /* Unsupported endianness. */
-      }
-      #if ARM_ENABLE_BIGENDIAN
-         __asm("SETEND LE");
-      #endif
-      return rc;
-   }
+   if (puts("") < 0 || fflush(0)) abort();
+   return EXIT_SUCCESS;
 }
